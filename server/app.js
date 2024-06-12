6aaf4d8c5bacc636aa4ef255604efeb2acb5804a
Ravindra162 <tarakaravindra.221cs162@nitk.edu.in>
2024-06-12 17:18:31 +0530

    cors update

--- server/app.js
+++ server/app.js
@@ -13,7 +13,10 @@ var app = express();
 const { client } = require('./db/db')
 
 // view engine setup
-app.use(cors())
+app.use(cors({
+  origin:'*',
+  methods:["GET","POST","PUT","DELETE"]
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
